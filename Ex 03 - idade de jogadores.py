977bf2cf384170bed48aa0b48e7edf674c2d2edb
Gabriel Firmino <gabrielfirmino033@gmail.com>
2023-06-12 22:53:29 -0300

    versao final ex03

--- Ex 03 - idade de jogadores.py
+++ Ex 03 - idade de jogadores.py
@@ -1,4 +1,28 @@
+#VERIFICADOR DE IDADE PARA JOGO (18 ANOS)
+
+
 from datetime import datetime
 
 hoje = datetime.now()
-print(hoje.date())
+nascimento = input('\033[1:4:35:40mInsira a data de nascimento (dd/mm/aa):\033[m ')
+nascimento = datetime.strptime(nascimento, '%d/%m/%Y')
+
+if hoje.month < nascimento.month or hoje.day < nascimento.day and hoje.month == nascimento.month:
+
+    idade = (hoje.year-nascimento.year-1)
+    print(idade)
+
+else:
+    idade = (hoje.year-nascimento.year)
+
+if idade>=18:
+    print('\033[1:37mPode jogar o jogo, bem-vindo 😉 !\033[m')
+else:
+    print(f'\033[1:37mInfelizmente você não pode jogar o jogo 😕 Você possui apenas {idade} anos !\033[m')
+
+
+
+
+
+
+
